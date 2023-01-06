79cd42c55b120e8109530ca0d8a09bd7be3bc2b1
fatih <34196005+fcakyon@users.noreply.github.com>
2023-01-06 23:55:37 +0300

    add automated language detection (#2)

--- README.md
+++ README.md
@@ -15,10 +15,40 @@ from safetext import SafeText
 st = SafeText(language='en')
 
 results = st.check_profanity(text='Some text with <profanity-word>.')
->> results
+results
 >> {'word': '<profanity-word>', 'index': 4, 'start': 15, 'end': 31}
 
 text = st.censor_profanity(text='Some text with <profanity-word>.')
->> text
+text
 >> "Some text with ***."
+```
+
+### automated language detection
+
+- automatically set language from text:
+
+```python
+from safetext import SafeText
+
+eng_text = "This story is about to take a dark turn."
+
+st = SafeText(language=None)
+st.set_language_from_text(eng_text)
+
+st.language
+>> 'en'
+```
+
+- automatically set language from .srt (subtitle) file:
+
+```python
+from safetext import SafeText
+
+turkish_srt_file_path = "turkish.srt"
+
+st = SafeText(language=None)
+st.set_language_from_srt(turkish_srt_file_path)
+
+st.language
+>> 'tr'
 ```
--- safetext/__init__.py
+++ safetext/__init__.py
@@ -1,14 +1,16 @@
+from safetext.utils import detect_language_from_srt, detect_language_from_text
 from .languages.en import EnglishProfanityChecker
 from .languages.tr import TurkishProfanityChecker
 
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 
 
 class SafeText:
     def __init__(self, language="en"):
         self.language = language
         self.checker = None
-        self.set_language(language)
+        if language is not None:
+            self.set_language(language)
 
     def set_language(self, language):
         self.language = language
@@ -19,12 +21,63 @@ class SafeText:
         else:
             raise ValueError("Language not supported")
 
+    def set_language_from_text(self, text):
+        """
+        Detects the language of the given text.
+
+        Args:
+            text (str): The text to detect the language of.
+
+        Returns:
+            str: The language code of the detected language.
+                (e.g. "en", "tr")
+        """
+        language = detect_language_from_text(text)
+        self.set_language(language)
+
+    def set_language_from_srt(self, srt_file, use_first_n_subs=10):
+        """
+        Detects the language of the given SRT file.
+
+        Args:
+            srt_file (str): The path to the SRT file to detect the language of.
+            use_first_n_subs (int): The number of subtitles to use for detection.
+
+        Returns:
+            str: The language code of the detected language.
+                (e.g. "en", "tr")
+        """
+        language = detect_language_from_srt(srt_file, use_first_n_subs)
+        self.set_language(language)
+
     def check_profanity(self, text):
+        """
+        Checks the given text for profanity.
+
+        Args:
+            text (str): The text to check for profanity.
+
+        Returns:
+            list: A list of profanity infos. Each profanity info is a dict with the following keys:
+                - word: The profanity word.
+                - index: The index of the profanity word in the text.
+                - start: The start index of the profanity word in the text.
+                - end: The end index of the profanity word in the text.
+        """
         if self.checker is None:
             raise ValueError("Language not set")
         return self.checker.check(text)
 
     def censor_profanity(self, text):
+        """
+        Censors the given text for profanity.
+
+        Args:
+            text (str): The text to censor for profanity.
+
+        Returns:
+            str: The censored text. The profanity words are replaced with asterisks.
+        """
         if self.checker is None:
             raise ValueError("Language not set")
         return self.checker.censor(text)
