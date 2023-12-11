ad6f5e6b86f6882dbd47a0e018ba90551122ca97
SeeknnDestroy <sarit17@itu.edu.tr>
2023-12-11 16:18:59 +0300

    move baseprofanity as profanity checker

--- safetext/__init__.py
+++ safetext/__init__.py
@@ -2,8 +2,6 @@ import os
 
 from safetext.utils import detect_language_from_srt, detect_language_from_text
 
-from .languages.base import BaseProfanityChecker
-
 __version__ = "0.0.4"
 
 
@@ -24,7 +22,7 @@ class SafeText:
             raise ValueError(f"No profanity word list found for language '{language}'.")
 
         self.language = language
-        self.checker = BaseProfanityChecker(language)
+        self.checker = ProfanityChecker(language)
 
     def _get_words_filepath(self, language: str) -> str:
         return os.path.join(os.path.dirname(__file__), f"languages/{language}/words.txt")
@@ -93,3 +91,84 @@ class SafeText:
     def _auto_set_language(self, text: str):
         detected_language = detect_language_from_text(text)
         self.set_language(detected_language)
+
+
+class ProfanityChecker:
+    """Base class for profanity checkers."""
+
+    def __init__(self, language):
+        self.language = language
+
+    @property
+    def words_filepath(self):
+        """Get the filepath for the profanity words file."""
+        import pathlib
+
+        return f"{pathlib.Path(__file__).parent.resolve()}/{self.language}/words.txt"
+
+    @property
+    def profanity_words(self):
+        """Get the profanity words for the language."""
+        if not hasattr(self, "_profanity_words"):
+            self._profanity_words = self._read_words(self.words_filepath)
+
+        return self._profanity_words
+
+    def _check(self, text):
+        """Check the text for profanity."""
+        # Split the text into a list of words
+        words = text.split()
+
+        # Initialize a list to store the indices of profanity words
+        profanity_infos = []
+
+        for i, word in enumerate(words):
+            if word.lower() in self.profanity_words:
+                start_index = sum(len(w) + 1 for w in words[:i])  # +1 to account for space between words
+                end_index = start_index + len(word)
+                profanity_info = {
+                    "word": word,
+                    "index": i + 1,
+                    "start": start_index,
+                    "end": end_index,
+                }
+                profanity_infos.append(profanity_info)
+
+        return profanity_infos
+
+    def _read_words(self, filepath):
+        """Read the profanity words from the given file."""
+        with open(filepath, encoding="utf8") as f:
+            profanity_words = f.read().splitlines()
+
+        return profanity_words
+
+    def _preprocess(self, text):
+        """Preprocess the text before checking for profanity."""
+        return text
+
+    def check(self, text):
+        """
+        Check the text for profanity.
+
+        Args:
+            text (str): The text to check for profanity.
+
+        Returns:
+            list: A list of profanity infos. Each profanity info is a dict with the following keys:
+                - word: The profanity word.
+                - index: The index of the profanity word in the text.
+                - start: The start index of the profanity word in the text.
+                - end: The end index of the profanity word in the text.
+        """
+        return self._check(self._preprocess(text))
+
+    def censor(self, text):
+        """Censor the text."""
+        detected_profanities = self.check(text)
+        for profanity in detected_profanities:
+            start_index = profanity["start"]
+            end_index = profanity["end"]
+            text = text.replace(text[start_index:end_index], "***")
+
+        return text
